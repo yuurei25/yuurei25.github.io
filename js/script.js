19757043a793c01d67f22922fef6b0f2bc45a879
yuurei25 <ibnu.abi.azzam.25@gmail.com>
2022-03-26 13:59:42 +0200

    Make CoreContent class

--- js/script.js
+++ js/script.js
@@ -1,74 +1,140 @@
-var converter = new showdown.Converter();
-var core = document.querySelector("div.core");
+class CoreContent {
+	#converter;
+	#directory;
+	container;
+	contentData;
+	currentContent;
+
+	get directory() {
+		return this.#directory;
+	}
+	set directory(directory) {
+		this.#directory = directory;
+		this.#setContentData();
+	}
+
+	constructor(container, directory, opts) {
+		this.#converter = new showdown.Converter();
+		this.#directory = directory;
+		this.container = container;
+		if (opts) {
+			if (opts.styles) this.styles = opts.styles;
+		}
+		this.#setContentData();
+	}
+
+	#setContentData() {
+		let client = new XMLHttpRequest();
+		let link = `${this.#directory}/data.json`;
+		client.open("GET", link);
+		client.onreadystatechange = (e) => {
+			if (client.readyState === 4 && client.status === 200) {
+				this.contentData = JSON.parse(client.responseText);
+				this.setContent(this.contentData[0]);
+			}
+		};
+		client.send();
+	}
+
+	setContent(content) {
+		let client = new XMLHttpRequest();
+		let link = `${this.#directory}/src/${content.permalink}`;
+		client.open("GET", link);
+		client.onreadystatechange = (e) => {
+			if (client.readyState === 4 && client.status === 200) {
+				let text = client.responseText;
+				let html = this.#converter.makeHtml(text);
+				this.container.innerHTML = html;
+				this.#embedClasses();
+				this.currentContent = content;
+			}
+		};
+		client.send();
+		return this;
+	}
+
+	#embedClasses() {
+		this.container.querySelectorAll("*").forEach((e) => {
+			let tagName = e.tagName.toLowerCase();
+			if (Object.keys(this.styles).includes(tagName)) {
+				coreStyle[tagName].forEach((e2, i) => e.classList.add(e2));
+			}
+		});
+		return this;
+	}
+}
+
+class NavigatedCoreContent extends CoreContent {
+	setNavigator(nextButton, prevButton) {
+		nextButton.addEventListener("click", (e) => {
+			this.next();
+		});
+
+		prevButton.addEventListener("click", (e) => {
+			this.previous();
+		});
+	}
+
+	next() {
+		let data = this.contentData;
+		let current = this.currentContent;
+		if (data[data.length - 1] === current) {
+			this.setContent(data[0]);
+		} else {
+			this.setContent(data[data.findIndex((e) => e === current) + 1]);
+		}
+	}
+
+	previous() {
+		let data = this.contentData;
+		let current = this.currentContent;
+		if (data[0] === current) {
+			this.setContent(data[data.length - 1]);
+		} else {
+			this.setContent(data[data.findIndex((e) => e === current) - 1]);
+		}
+	}
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// ---------------- SCRIPT ----------------
+
+let core = document.querySelector("div.core");
 
 let coreStyle = {
 	h1: ["text-center", "p-2", "mb-3", "rounded", "text-light", "bg-dark"],
 	p: [],
 };
 
-let data;
-let currentContent;
+let coreContent = new NavigatedCoreContent(core, "articles", {
+	styles: coreStyle,
+});
 
-(function () {
-	let client = new XMLHttpRequest();
-	client.open("GET", "articles/data.json");
-	client.onreadystatechange = (e) => {
-		if (client.readyState === 4 && client.status === 200) {
-			let response = client.responseText;
-			console.log(response);
-			data = JSON.parse(response);
-			setCoreContent(data[0]);
-		}
-	};
-	client.send();
-})();
-
-document
-	.querySelector(".content-switch-next")
-	.addEventListener("click", (e) => {
-		if (data[data.length - 1] === currentContent) {
-			setCoreContent(data[0]);
-		} else {
-			setCoreContent(
-				data[data.findIndex((e) => e === currentContent) + 1]
-			);
-		}
-	});
-
-document
-	.querySelector(".content-switch-previous")
-	.addEventListener("click", (e) => {
-		if (data[0] === currentContent) {
-			setCoreContent(data[data.length - 1]);
-		} else {
-			setCoreContent(
-				data[data.findIndex((e) => e === currentContent) - 1]
-			);
-		}
-	});
-
-function setCoreContent(content) {
-	// core.innerHTML = "";
-	let client = new XMLHttpRequest();
-	client.open("GET", `articles/src/${content.permalink}`);
-	client.onreadystatechange = (e) => {
-		if (client.readyState === 4 && client.status === 200) {
-			let text = client.responseText;
-			let html = converter.makeHtml(text);
-			core.innerHTML = html;
-			embedClasses(core);
-			currentContent = content;
-		}
-	};
-	client.send();
-}
-
-function embedClasses(node) {
-	console.log(node.querySelectorAll("*"));
-	node.querySelectorAll("*").forEach((e) => {
-		let tagName = e.tagName.toLowerCase();
-		if (Object.keys(coreStyle).includes(tagName)) {
-			coreStyle[tagName].forEach((e2, i) => e.classList.add(e2));
-		}
-	});
+setNavigator();
+function setNavigator() {
+	let nextBtn = document.querySelector(".content-switch-next");
+	let prevBtn = document.querySelector(".content-switch-previous");
+	coreContent.setNavigator(nextBtn, prevBtn);
 }
